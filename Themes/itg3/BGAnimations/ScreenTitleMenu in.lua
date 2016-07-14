7e55cdf65584e900eccb8cba1064d081e7c04385
Retropony <retropone@derpymail.org>
2016-07-14 22:43:56 +0200

    Add files via upload
    Add the intro sound

--- Themes/itg3/BGAnimations/ScreenTitleMenu in.lua
+++ Themes/itg3/BGAnimations/ScreenTitleMenu in.lua
@@ -3,6 +3,7 @@ local t = Def.ActorFrame{
 		InitCommand=cmd(Center;FullScreen;);
 		OnCommand=cmd(linear,0.3;diffusealpha,0);
 	};
+	LoadActor(THEME:GetPathS("","_logo"))..{ OnCommand=cmd(play); };
 };
 
 return t;
