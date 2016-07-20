270f6786bfc3693da81f5943f324fe7e39b643cd
Retropony <retropone@derpymail.org>
2016-07-20 12:28:02 +0200

    Add the cancel sound

--- Themes/itg3/BGAnimations/Screen Cancel/default.lua
+++ Themes/itg3/BGAnimations/Screen Cancel/default.lua
@@ -3,4 +3,7 @@ return Def.ActorFrame{
 		InitCommand=cmd(Center;FullScreen;diffuse,color("#000000");cropleft,1;fadeleft,.5;);
 		OnCommand=cmd(linear,0.5;cropleft,-0.5);
 	};
+	LoadActor("cancel") .. {
+		StartTransitioningCommand=cmd(play);
+	};
 };
